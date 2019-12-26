b7b1009c473e5d8fad250ff1fdee755ad0132286
Umer Khan <umerkk164@gmail.com>
2019-12-26 10:58:12 +0500

    Updates

---
+++ precache-manifest.4a0af832184cae8325e76345d1097cc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e02b148ffdfe009db28e032d04e774",
+    "url": "/watchtoday/index.html"
+  },
+  {
+    "revision": "bf11d02f19200f2ac93b",
+    "url": "/watchtoday/static/css/main.48d3346d.chunk.css"
+  },
+  {
+    "revision": "efe9998fae035f6c21f2",
+    "url": "/watchtoday/static/js/2.0235b1fe.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/watchtoday/static/js/2.0235b1fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf11d02f19200f2ac93b",
+    "url": "/watchtoday/static/js/main.3bd0124f.chunk.js"
+  },
+  {
+    "revision": "2d94a3631b8a48fa88c5",
+    "url": "/watchtoday/static/js/runtime-main.d632f303.js"
+  },
+  {
+    "revision": "7d02b50b228d18e7da9e43b122aa2d84",
+    "url": "/watchtoday/static/media/loading.7d02b50b.gif"
+  }
+]);
